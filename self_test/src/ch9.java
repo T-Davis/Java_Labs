b433d5fa9a44f70ea3b77f3c4a2c6c1a679a8c2c
Trevor Davis <tzendou@gmail.com>
2017-04-03 13:17:43 +0700

    progress

---
+++ self_test/src/ch9.java
@@ -0,0 +1,22 @@
+/**
+ * Created by tdavis on 3/29/17.
+ *
+ * 1. Throwable
+ * 2. try is kind of like an if statement, if this works (try this) move on, if not catch exception
+ * 3. your not calling try
+ * 4. your program stops and you get the default error handling by the jvm
+ * 5. the super class will catch all the exceptions, no need for the second catch. Call b first, you want to catch
+ *      exceptions closest to where it happened
+ * 6. yes
+ * 7. finally is what is executed at the end of a try block
+ * 8. everything except runtime and error
+ * 9. does not extend throwable
+ * 10. later
+ * 11. error in jvm, your code, or throw statement
+ * 12. error and exception
+ * 13. can catch multiple exceptions
+ * 14. no, that's what the jvm does
+ *
+ */
+public class ch9 {
+}
